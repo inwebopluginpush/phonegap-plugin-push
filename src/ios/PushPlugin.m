504ce2add5b9ccbe0c9eaae46c6ee13e7fda36d7
steven <lhelgoualch.steven@gmail.com>
2018-06-06 15:30:17 +0200

    Update PushPlugin.m

--- src/ios/PushPlugin.m
+++ src/ios/PushPlugin.m
@@ -482,8 +482,6 @@
             [additionalData setObject:[NSNumber numberWithBool:NO] forKey:@"coldstart"];
         }
       
-        [message setObject:"inwebo" forKey:@"title"];
-      
         [message setObject:additionalData forKey:@"additionalData"];
 
         // send notification message
